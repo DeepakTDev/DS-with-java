31f9469ce20d38559bfc391878f047865f02b7fc
DeepakTDev <42795737+DeepakTDev@users.noreply.github.com>
2021-06-04 17:53:25 +0530

    Add valid BST tree

--- BinaryTreeDiameter.java
+++ BinaryTreeDiameter.java
@@ -3,20 +3,27 @@ import java.util.*;
 public class BinaryTreeDiameter {
     BinaryNode root;
 
+    BinaryNode prev;
+
     int diameter = 0;
     public static void main(String args[])
     {
         BinaryTreeDiameter bt = new BinaryTreeDiameter();
 
-        bt.root = new BinaryNode(1);
+        bt.root = new BinaryNode(4);
 
-        bt.root.setLeftNode(new BinaryNode(2));
-        bt.root.getLeftNode().setLeftNode(new BinaryNode(4));
-        bt.root.setRightNode(new BinaryNode(3));
-        bt.root.getRightNode().setRightNode(new BinaryNode(5));
+        bt.root.setLeftNode(new BinaryNode(3));
+        bt.root.getLeftNode().setLeftNode(new BinaryNode(1));
+        bt.root.setRightNode(new BinaryNode(5));
+        bt.root.getRightNode().setRightNode(new BinaryNode(6));
 
         bt.traverse();
         bt.getDiameter();
+        if(bt.isBST()) {
+            System.out.println("valid BST");
+        } else {
+            System.out.println("invalid BST");
+        }
     }
 
     public void getDiameter()
@@ -43,6 +50,30 @@ public class BinaryTreeDiameter {
 
     }
 
+    public boolean isBST() {
+        return this.checkBST(this.root);
+    }
+
+    public boolean checkBST(BinaryNode node) {
+
+        if (node != null) {
+
+            if (!checkBST(node.getLeftNode())) {
+                return false;
+            }
+
+            if (prev != null && prev.getData() >= node.getData()) {
+                return  false;
+            }
+
+            this.prev = node;
+
+            return checkBST(node.getRightNode());
+        }
+
+        return  true;
+
+    }
     private void inOrderTraverse(BinaryNode node)
     {
         if (node == null) {
